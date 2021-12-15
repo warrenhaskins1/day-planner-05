682ca750986b708892892dc3cf89e4ca552d24b3
Warren Haskins <warrenhaskins1@gmail.com>
2021-12-15 16:03:05 -0500

    html initial structure

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+//1. WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar
+
+//2. WHEN I scroll down
+// THEN I am presented with timeblocks for standard business hours
+
+//3. WHEN I view the timeblocks for that day
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+
+//4. WHEN I click into a timeblock
+// THEN I can enter an event
+
+//5. WHEN I click the save button for that timeblock
+// THEN the text for that event is saved in local storage
+
+//6. WHEN I refresh the page
+// THEN the saved events persist
